b021ff6548ae0f21b37e3cbe6c6106eb1211b1c2
justshreyas <professionally.shreyas@gmail.com>
2022-08-18 16:57:16 +0530

    TASK : replace aar urls

--- bin/unzip_zoom_sdk.dart
+++ bin/unzip_zoom_sdk.dart
@@ -66,19 +66,17 @@ Future<void> checkAndDownloadSDK(String location) async {
   var androidCommonLibFile = location + '/android/libs/commonlib.aar';
   exists = await File(androidCommonLibFile).exists();
   if (!exists) {
-    await downloadFile(
-        Uri.parse("https://www.dropbox.com/s/fxiqvt6j07pf2cr/commonlib.aar?dl=1"
-            // 'https://www.dropbox.com/s/i5fww50elzrphra/commonlib.aar?dl=1'
-            ),
+    await downloadFile(Uri.parse(
+            // "https://www.dropbox.com/s/fxiqvt6j07pf2cr/commonlib.aar?dl=1"
+            'https://www.dropbox.com/s/i5fww50elzrphra/commonlib.aar?dl=1'),
         androidCommonLibFile);
   }
   var androidRTCLibFile = location + '/android/libs/mobilertc.aar';
   exists = await File(androidRTCLibFile).exists();
   if (!exists) {
-    await downloadFile(
-        Uri.parse("https://www.dropbox.com/s/e9f7qz2ajk42tft/mobilertc.aar?dl=1"
-            // 'https://www.dropbox.com/s/ahh06pva216szc1/mobilertc.aar?dl=1'
-            ),
+    await downloadFile(Uri.parse(
+            // "https://www.dropbox.com/s/e9f7qz2ajk42tft/mobilertc.aar?dl=1"
+            'https://www.dropbox.com/s/ahh06pva216szc1/mobilertc.aar?dl=1'),
         androidRTCLibFile);
   }
 }

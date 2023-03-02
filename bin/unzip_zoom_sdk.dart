4f6d2ab0aeafb48e57d9c550f960e5924fe33275
justshreyas <professionally.shreyas@gmail.com>
2023-03-02 15:11:05 +0530

    TASK : update to android zoom sdk 5.13.3

--- bin/unzip_zoom_sdk.dart
+++ bin/unzip_zoom_sdk.dart
@@ -49,7 +49,8 @@ Future<void> checkAndDownloadSDK(String location) async {
 
   if (!exists) {
     await downloadFile(
-        Uri.parse( // ! at the end of the url, ensure "?dl=1" 
+        Uri.parse(
+          // ! at the end of the url, ensure "?dl=1"
           // "https://www.dropbox.com/s/eq9yw94uwqx2tl2/MobileRTC%20Five%20Ten%203?dl=1", //5.10.3
           // "https://www.dropbox.com/s/gfkj4hewwnb0qwa/MobileRTC%20Five%20Eleven%203?dl=1", //5.11.3
           // 'https://www.dropbox.com/s/q7l6ryp870ggxx5/MobileRTC?dl=1'
@@ -73,7 +74,9 @@ Future<void> checkAndDownloadSDK(String location) async {
   exists = await File(androidCommonLibFile).exists();
   if (!exists) {
     await downloadFile(
-        Uri.parse("https://www.dropbox.com/s/fxiqvt6j07pf2cr/commonlib.aar?dl=1"
+        Uri.parse(
+            "https://www.dropbox.com/s/pc5h8dxkf5t1p0j/commonlib%20-%20five%20eleven%20ten%20-%20aar.aar?dl=1" // 5.13.10
+            // "https://www.dropbox.com/s/fxiqvt6j07pf2cr/commonlib.aar?dl=1"
             // 'https://www.dropbox.com/s/i5fww50elzrphra/commonlib.aar?dl=1'
             ),
         androidCommonLibFile);
@@ -82,9 +85,11 @@ Future<void> checkAndDownloadSDK(String location) async {
   exists = await File(androidRTCLibFile).exists();
   if (!exists) {
     await downloadFile(
-        Uri.parse("https://www.dropbox.com/s/e9f7qz2ajk42tft/mobilertc.aar?dl=1"
-            // 'https://www.dropbox.com/s/ahh06pva216szc1/mobilertc.aar?dl=1'
-            ),
+        Uri.parse(
+          "https://www.dropbox.com/s/3qmscuct756nhp0/mobilertc%20-%20five%20eleven%20ten%20-%20aar.aar?dl=1", // 5.13.10
+          // "https://www.dropbox.com/s/e9f7qz2ajk42tft/mobilertc.aar?dl=1"
+          // 'https://www.dropbox.com/s/ahh06pva216szc1/mobilertc.aar?dl=1'
+        ),
         androidRTCLibFile);
   }
 }
